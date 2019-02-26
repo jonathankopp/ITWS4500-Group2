40042b31e986522fc1db3aab1b55832b151d311b
Honghao Li <lihonghao2010@gmail.com>
2019-02-25 17:13:19 -0800

    PostPicture css updated, and now the page could be viewed at localhost:3000

--- server.js
+++ server.js
@@ -2,13 +2,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-
+app.use('/public',express.static('public'));
 //Point to HTML file
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 //Allow static files
-app.use(express.static('public'));
-
 
 app.listen(port, () => console.log('Example app listening on port ' + port));
