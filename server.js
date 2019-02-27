d7ef03120ca1667784f153a9a6cc3fa51332d5b1
hangyenli <lihonghao2010@gmail.com>
2019-02-27 16:47:40 -0500

    update file structure and index page

--- server.js
+++ server.js
@@ -1,59 +1,139 @@
-// server init + mods
-const express = require('express');
-const app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
-const port = 3000;
+var express = require('express'); // Express web server framework
+var request = require('request'); // "Request" library
+var cors = require('cors');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+
+var client_id = '1d919a5a30ef4be69dd04141c730e3a9'; // Your client id
+var client_secret = 'a2c8d9a1fe2b44e6a0d6012c2e0a9222'; // Your secret
+var redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+
+var generateRandomString = function(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+};
+
+var stateKey = 'spotify_auth_state';
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'))
+    .use(cors())
+    .use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+    var state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // your application requests authorization
+    var scope = 'user-read-private user-read-email user-top-read';
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
+});
+
+app.get('/callback', function(req, res) {
+
+    // your application requests refresh and access tokens
+    // after checking the state parameter
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+    var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    if (state === null || state !== storedState) {
+        res.redirect('/#' +
+            querystring.stringify({
+                error: 'state_mismatch'
+            }));
+    } else {
+        res.clearCookie(stateKey);
+        var authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            form: {
+                code: code,
+                redirect_uri: redirect_uri,
+                grant_type: 'authorization_code'
+            },
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            },
+            json: true
+        };
+
+        request.post(authOptions, function(error, response, body) {
+            if (!error && response.statusCode === 200) {
+
+                var access_token = body.access_token,
+                    refresh_token = body.refresh_token;
+
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                };
+
+                // use the access token to access the Spotify Web API
+                request.get(options, function(error, response, body) {
+                    console.log(body);
+                });
+
+                // we can also pass the token to the browser to make requests from there
+                res.redirect('/#' +
+                    querystring.stringify({
+                        access_token: access_token,
+                        refresh_token: refresh_token
+                    }));
+            } else {
+                res.redirect('/#' +
+                    querystring.stringify({
+                        error: 'invalid_token'
+                    }));
+            }
+        });
+    }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+    // requesting access token from refresh token
+    var refresh_token = req.query.refresh_token;
+    var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+        form: {
+            grant_type: 'refresh_token',
+            refresh_token: refresh_token
+        },
+        json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var access_token = body.access_token;
+            res.send({
+                'access_token': access_token
+            });
+        }
+    });
+});
 
 //Allow static files
 app.use('/public',express.static('public'));
+app.use('/api',express.static('api'));
 
 //Point to HTML file
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
-// connect to mongodb
-var db = mongoose.connection;
-db.on('error', console.error);
-mongoose.connect('mongodb://localhost/mychat');
-
-// mongodb schemas
-var chatMessage = new mongoose.Schema({
-    username: String,
-    message: String
-});
-var Message = mongoose.model('Message', chatMessage);
-
-// user connected even handler
-io.on('connection', function(socket){
-
-    // log & brodcast connect event
-    console.log('a user connected');
-
-    // log disconnect event
-    socket.on('disconnect', function(){
-        console.log('user disconnected');
-    });
-
-    // message received event handler
-    socket.on('message', function(msg){
-        // log chat msg
-        console.log('message: ' + msg);
-
-        // broadcast chat msg to others
-        socket.broadcast.emit('message', msg);
-
-        // save message to db
-        var message = new Message ({
-            message : msg
-        });
-        message.save(function (err, saved) {
-            if (err) {
-                return console.log('error saving to db');
-            }
-        })
-    });
-});
-
-
-app.listen(port, () => console.log('Example app listening on port ' + port));
+app.listen(3000);
